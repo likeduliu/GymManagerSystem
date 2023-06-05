59aa787624a08598c87b9735f7900a83d41992df
likeduliu <1822497173@qq.com>
2023-06-05 15:40:15 +0800

    6-5

--- vue/gym-vue/src/components/SideNav.vue
+++ vue/gym-vue/src/components/SideNav.vue
@@ -37,7 +37,7 @@
         <el-menu-item index="/field/CancleBook">取消预约</el-menu-item>
         <el-menu-item index="/field/UpdateBook">场地预约修改</el-menu-item>
         <!-- <el-menu-item index="/field/QueryWeek">场地一周信息查询</el-menu-item> -->
-        <el-menu-item index="/field/Use">使用场地</el-menu-item>
+        <el-menu-item index="/field/Use">场地费用结算</el-menu-item>
         <!-- <el-menu-item index="/field/Reserve">校队预留场地</el-menu-item> -->
         <!-- <el-menu-item index="/field/Rate">场地收费标准查询</el-menu-item> -->
         <el-menu-item index="/field/Notice">场地公告管理</el-menu-item>
--- vue/gym-vue/src/views/CDGL/CdglUseView.vue
+++ vue/gym-vue/src/views/CDGL/CdglUseView.vue
@@ -4,13 +4,22 @@
           
             <el-main>
                 <el-table :data="tableData">
-                    <el-table-column label="添加场地" width="140">
+                    <el-table-column label="场地费用结算" width="140">
                     </el-table-column>
                     <el-table-column prop="name" label="姓名" width="120">
                     </el-table-column>
                     <el-table-column prop="address" label="地址">
                     </el-table-column>
                 </el-table>
+                <template>
+                  <div>
+                    <p>计时器：{{ formattedTime }}</p>
+                        <button @click="startTimer" :disabled="isTimerRunning">开始</button>
+                        <button @click="pauseTimer" :disabled="!isTimerRunning">暂停</button>
+                        <button @click="endTimer" :disabled="!isTimerRunning">结束</button>
+                        <p>费用：{{ calculateFee() }}</p>
+                  </div>
+                </template>
 
                 
             </el-main>
@@ -23,13 +32,67 @@
 
 <script>  
     export default {
+        data() {
+    return {
+      startTime: null, // 计时器开始时间
+      endTime: null, // 计时器结束时间
+      isTimerRunning: false // 计时器是否正在运行
+    };
+  },
+  computed: {
+    formattedTime() {
+      if (this.startTime && !this.endTime) {
+        // 计时器正在运行中
+        const currentTime = new Date().getTime();
+        const elapsedMilliseconds = currentTime - this.startTime;
+        return this.formatTime(elapsedMilliseconds);
+      } else if (this.startTime && this.endTime) {
+        // 计时器已结束
+        const elapsedMilliseconds = this.endTime - this.startTime;
+        return this.formatTime(elapsedMilliseconds);
+      } else {
+        // 计时器未开始
+        return '00:00:00';
+      }
+    }
+  },
         methods: {
-            handleSizeChange(val) {
-                console.log(`每页 ${val} 条`);
+            formatTime(milliseconds) {
+              const seconds = Math.floor((milliseconds / 1000) % 60);
+              const minutes = Math.floor((milliseconds / (1000 * 60)) % 60);
+              const hours = Math.floor((milliseconds / (1000 * 60 * 60)) % 24);
+              return `${this.padTime(hours)}:${this.padTime(minutes)}:${this.padTime(seconds)}`;
             },
-            handleCurrentChange(val) {
-                console.log(`当前页: ${val}`);
-            }
+            padTime(time) {
+              return time.toString().padStart(2, '0');
+            },
+            startTimer() {
+              this.startTime = new Date().getTime();
+              this.endTime = null;
+              this.isTimerRunning = true;
+            },
+            pauseTimer() {
+              this.endTime = new Date().getTime();
+              this.isTimerRunning = false;
+            },
+            endTimer() {
+              this.endTime = new Date().getTime();
+              this.isTimerRunning = false;
+              this.saveRecord(); // 保存记录和计算费用
+            },
+            calculateFee() {
+              if (this.startTime && this.endTime) {
+                const elapsedMilliseconds = this.endTime - this.startTime;
+                // 根据你的收费策略进行费用计算
+                // 这里只是一个示例，你需要根据实际情况进行调整
+                const hours = Math.ceil(elapsedMilliseconds / (1000 * 60 * 60));
+                const ratePerHour = 10; // 每小时费率
+                const fee = hours * ratePerHour;
+                return fee;
+              }
+              return 0;
+            },
+            
         },       
     }
 </script>
